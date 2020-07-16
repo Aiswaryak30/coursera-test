cd2b7c91dd99bebd28d8ed7c43cb82ff6d51d491
Aiswarya <aisuaizee@gmail.com>
2020-07-16 16:57:54 +0530

    module1 assignment

--- module1-assignment/app.js
+++ module1-assignment/app.js
@@ -1,10 +1,9 @@
 (function(){
 'use strict';
-angular.module('LunchCheck',[])
-.controller('LunchCheckController',LunchCheckController)
 
-LunchCheckController.$inject=['$scope']
-function LunchCheckController($scope){
-    $scope.name="Aisu"
-}
+angular.module('LunchCheck',[])
+
+.controller('LunchCheckController',function LunchCheckController($scope){
+    $scope.name='Aisu'
 })
+})();
