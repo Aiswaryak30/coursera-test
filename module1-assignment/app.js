6e87e4e585e8b23abc7db78b549d835664234c8a
Aiswarya <aisuaizee@gmail.com>
2020-07-17 17:34:07 +0530

    assignment1

--- module1-assignment/app.js
+++ module1-assignment/app.js
@@ -3,7 +3,35 @@
 
 angular.module('LunchCheck',[])
 
-.controller('LunchCheckController',function LunchCheckController($scope){
-    $scope.name='Aiswarya'
-})
+.controller('LunchCheckController',LunchCheckController);
+
+LunchCheckController.$inject=[$scope]
+function LunchCheckController($scope){
+   $scope.item=""
+   $scope.check=function(){
+   if($scope.item=="")
+   {
+      $scope.colors="color1"
+      $scope.message="Please enter data first"
+       }
+    else
+    {
+      const words = $scope.item.split(',');
+      console.log(words);
+      if(words.length <= 3)
+       {
+          $scope.colors="color2"
+          $scope.message="Enjoy!"
+            }
+       else
+        {
+           $scope.colors="color2"
+           $scope.message="Too much!"
+           }        
+        }
+       }
+       }
 })();
+
+
+
